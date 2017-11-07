92785b293124ff8eb246697e82a7881ddbbb5e93
ekraihan <ekraihan@iupui.edu>
2017-11-07 00:44:31 -0500

    adding skill model

--- app/models/profile.js
+++ app/models/profile.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  skills: DS.hasMany(),
+  about: DS.attr('string')
+  education: DS.attr('string'),
+  birthdate: DS.attr('string')
 });
