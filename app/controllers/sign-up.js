58dd1ec771194b2222d591dadcb30d84e8462c5a
Jeremy Brown <brown471@iupui.edu>
2017-11-10 23:59:36 -0500

    adding in gatekeeper code to signup.js

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -3,9 +3,17 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
     createAccount () {
+      let {email, username, password} = this.getProperties (['email', 'username', 'password']);
+      let account = this.get ('store').createRecord ('account', {username, password, email});
+      let adapterOptions = {signIn: true};
 
+      account.save ({adapterOptions}).then (account => {
+        Ember.Logger.log(account)
+      }).catch (reason => {
+        Ember.Logger.log(reason)
+      });
     },
-    
+
     signUpClick(){
 
     }
