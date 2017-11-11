d39691540e0ea50f2255e17712d8f62aca301cb5
Jeremy Brown <brown471@iupui.edu>
2017-11-10 22:18:44 -0500

    emptying signup functions because they were failing

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -3,9 +3,9 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
     createAccount () {
-      let {email, username, password} = this.getProperties (['email', 'username', 'password']);
-      let adapterOptions = {signIn: true};
+
     },
+    
     signUpClick(){
 
     }
