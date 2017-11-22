0d6a392eb6ea9204b78ba498a95a575d9aee1e5c
ekraihan <ekraihan@iupui.edu>
2017-11-22 00:48:55 -0500

    adding nav bar

---
+++ app/controllers/application.js
@@ -0,0 +1,17 @@
+import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+
+export default Controller.extend({
+  /// Routes where we don't want to show the application template
+  nonApplicationRoutes: ['sign-in', 'sign-up'],
+
+  /**
+   * Return a boolean saying whether or not the application template
+   * should be displayed
+   */
+  showApplicationTemplate: computed('currentRouteName', {
+    get() {
+      return this.get('nonApplicationRoutes').every(route => this.get('currentRouteName') !== route);
+    }
+  })
+});
