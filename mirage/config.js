69e1fa0507c0486a20e49bade9c1fbbd3fba371f
ekraihan <ekraihan@iupui.edu>
2017-11-22 23:46:12 -0500

    mirage experiment

--- mirage/config.js
+++ mirage/config.js
@@ -6,55 +6,88 @@ export default function() {
 
   this.get('/users', () => {
     return {
-        "data": {
-          "id": "5a15da06096b9300013f43a3",
-          "type": "User",
-          "attributes": {
-              "_id": "5a15da06096b9300013f43a3",
-              "city": "Chicago",
-              "state": "IL",
-              "radius": 829,
-              "profileId": "5a15da06096b9300013f43a4",
-              "__v": 0
-          },
+      "data": {
+        "id": "5a15da06096b9300013f43a3",
+        "type": "user",
+        "attributes": {
+          "city": "Chicago",
+          "state": "IL",
+          "radius": 829
         },
         "relationships": {
-            "profile": {
-                "data": {
-                    "id": "5a15da06096b9300013f43a4",
-                    "type": "Profile",
-                },
-            }
+          "profile": {
+            "data": { "id": "5a15da06096b9300013f43a4", "type": "Profile" }
+          }
+        }
+      },
+      "included": [{
+        "type": "profile",
+        "id": "5a15da06096b9300013f43a4",
+        "attributes": {
+          "isNull": true,
+          "about": "I am a developer",
+          "birthdate": "10/10/2010"
         },
-       "included": [
-         {
-         "type": "Profile",
-         "id": "5a15da06096b9300013f43a4",
-         "attributes": {
-             "_id": "5a15da06096b9300013f43a4",
-             "isNull": true,
-             "__v": 0
-           },
-         },
-
-         {
-         "type": "Skill",
-         "id": "3123213",
-         "attributes": {
-             "_id": "3123213",
-             "name": "Baking",
+        "relationships": {
+          "skills": {
+            "data" : [
+              { "type": "skill", "id": "3123213" },
+              { "type": "skill", "id": "1234" }
+            ]
           },
-         "relationships": {
-            "profile": {
-                "data" : {
-                "type": "profile",
-                "id": "5a15da06096b9300013f43a4",
-               },
-            },
+          "occupations": {
+            "data" : [
+              { "type": "occupation", "id": "4321" }
+            ]
           },
-         },
-       ]
-      }
+          "education": {
+            "data" : [
+              { "type": "education", "id": "2222" },
+              { "type": "education", "id": "1111" }
+            ]
+          }
+        }
+      },
+      {
+        "type": "skill",
+        "id": "3123213",
+        "attributes": {
+          "name": "Baking"
+        }
+      },
+      {
+        "type": "skill",
+        "id": "1234",
+        "attributes": {
+          "name": "Flipping Pancakes"
+        }
+      },
+      {
+        "type": "occupation",
+        "id": "4321",
+        "attributes": {
+          "name": "DB Admin"
+        }
+      },
+      {
+        "type": "education",
+        "id": "1111",
+        "attributes": {
+          "name": "IUPUI",
+          "title": "BS Computer Science",
+          "location": "Indianapolis, IN"
+        }
+      },
+      {
+        "type": "education",
+        "id": "2222",
+        "attributes": {
+          "name": "Colombia University",
+          "title": "BS Electrical Engineering",
+          "location": "Colombia"
+        }
+      }]
+    }
 });
 
   // this.post('/users', (schema,req) => {
