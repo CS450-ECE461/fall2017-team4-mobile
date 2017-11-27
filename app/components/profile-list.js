d4110d8eb83e72ba2be8eb841fbc4d3af4e4f7a7
John B <j42b2014@gmail.com>
2017-11-26 19:04:58 -0500

    fix component for travis CI build and some indentations for readability

--- app/components/profile-list.js
+++ app/components/profile-list.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  imageSrc: '',
+  jobName: '',
+  dateRange: '',
+  companyName: '',
+  location: '',
+  buttonText: '',
 });
