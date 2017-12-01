1709767f5b04623d162a7fb63199c94c64a6c30f
Jeremy Brown <brown471@iupui.edu>
2017-11-30 23:44:08 -0500

    adding actions for header to the search controller

--- app/controllers/detailed-search.js
+++ app/controllers/detailed-search.js
@@ -1,7 +1,25 @@
 import Controller from '@ember/controller';
+import Ember from "ember";
 
 export default Controller.extend({
   appController: Ember.inject.controller('application'),
   resultsController: Ember.inject.controller('detailed-routes.search-results'),
-  searchesController: Ember.inject.controller('detailed-routes.searches')
+  searchesController: Ember.inject.controller('detailed-routes.searches'),
+
+  searchBarText: "Job title or company",
+  searchValue: '',
+
+  actions: {
+    onClickFilter() {
+      Ember.logger.log("Filtering Job Search");
+    },
+
+    onClickBack() {
+      Ember.logger.log("Back Button Clicked");
+    },
+
+    onSearchBarInput(value) {
+      Ember.logger.log(value);
+    }
+  }
 });
