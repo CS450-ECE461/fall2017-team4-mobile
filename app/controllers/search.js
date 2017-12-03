79ab16d67fa9477faf512035703ec34bd771393a
John B <j42b2014@gmail.com>
2017-12-02 19:30:50 -0500

    utilize helper for image assets

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -6,15 +6,15 @@ export default Controller.extend({
   actions:{
     showRecommendedJobSlider: function(){
 
-      Ember.$( "#recommended-jobs-body" ).slideToggle( "medium");
+      Ember.$( "#recommended-jobs-body" ).slideToggle( "fast");
 
       if(Ember.$("#recommended-jobs-body").hasClass('hide')){
-          Ember.$('#recommended-jobs-header').animate({bottom: '50px'}, 'medium');
+          Ember.$('#recommended-jobs-header').animate({bottom: '50px'}, 'fast');
           Ember.$("#recommended-jobs-body").removeClass('hide').addClass('show');
           Ember.$("#recommended-jobs-title").text("Our Top Picks for You. ");
       }
       else {
-          Ember.$('#recommended-jobs-header').animate({bottom: '525px'}, 'medium');
+          Ember.$('#recommended-jobs-header').animate({bottom: '525px'}, 'fast');
           Ember.$("#recommended-jobs-body").removeClass('show').addClass('hide');
           Ember.$("#recommended-jobs-title").text("Not So Lucky?");
       }
