8aaf11fe7cc92a3be2d8a03f79d4964b12c85706
Jeremy Brown <brown471@iupui.edu>
2017-12-04 13:40:36 -0500

    a couple new changes

--- app/components/job-search-filter.js
+++ app/components/job-search-filter.js
@@ -2,5 +2,20 @@ import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['job-search-filter'],
-  title: ''
+  searchText: '',
+  searchLocation: '',
+  image: '',
+
+  clickRemove: null,
+  clickOpen: null,
+
+  actions: {
+    onClickRemove() {
+      this.get('clickRemove')();
+    },
+
+    onClickOpen() {
+      this.get('clickOpen')();
+    }
+  }
 });
