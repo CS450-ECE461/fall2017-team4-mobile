d909a574824eab75996cfca13fcf0692cc746d2e
John B <j42b2014@gmail.com>
2017-12-06 06:24:04 -0500

    need sleep

--- mirage/config.js
+++ mirage/config.js
@@ -1,5 +1,4 @@
 import ENV from '../config/environment';
-import { faker} from 'ember-cli-mirage';
 
 export default function() {
   this.urlPrefix = `${ENV.APP.API}`;
@@ -67,54 +66,6 @@ export default function() {
     }
   );
 
-  this.get('/jobs', {
-      data: [
-        {
-          type: "job",
-          id: "1",
-          attributes: {
-            title: "Software Developer",
-            location: "Indianapolis, IN"
-          },
-          relationships: {
-            company: {
-              data: {type: "company", id: "1"}
-            }
-          }
-        },
-        {
-          type: "job",
-          id: "2",
-          attributes: {
-            title: "Java Guru",
-            location: "San Fransisco, CA"
-          },
-          relationships: {
-            company: {
-              data: {type: "company", id: "2"}
-            }
-          }
-        }
-      ],
-      included: [
-        {
-          type: "company",
-          id: "1",
-          attributes: {
-            name: "Salesforce",
-            logo: faker.image.business()
-          }
-        },
-        {
-          type: "company",
-          id: "2",
-          attributes: {
-            name: "Genesys",
-            logo: faker.image.business()
-          }
-        }
-      ]
-  });
 
   this.passthrough('http://165.227.76.52:5000/**');
 }
