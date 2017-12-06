691a7f64b14ca3528ee51625f63b9e8e6cbe3199
ekraihan <ekraihan@iupui.edu>
2017-12-05 23:19:22 -0500

    starting get chats

--- app/adapters/chat.js
+++ app/adapters/chat.js
@@ -2,7 +2,12 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   query(store, type, query) {
-    let url = `${this.get('host')}/${this.get('namespace')}/chats/${query.id}`;
-    return this.ajax(url, 'GET');
+    if (query.userId !== undefined) {
+      let url = `${this.get('host')}/${this.get('namespace')}/users/${query.userId}/chats`;
+      return this.ajax(url, 'GET');
+    } else {
+      let url = `${this.get('host')}/${this.get('namespace')}/chats/${query.id}`;
+      return this.ajax(url, 'GET');
+    }
   }
 });
