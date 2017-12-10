1113ecf9d0cf4ef5e5a21ad7825c122f20332de2
ekraihan <ekraihan@iupui.edu>
2017-12-10 14:00:37 -0500

    removing silly comment

--- tests/unit/routes/chat/chat-test.js
+++ tests/unit/routes/chat/chat-test.js
@@ -2,9 +2,6 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:chat/chat', 'Unit | Route | chat/chat', {
   // Specify the other units that are required for this test.
-  // needs: ['service:gatekeeper']
-
-  // gatekeeper: "wassup"
 });
 
 test('it exists', function(assert) {
