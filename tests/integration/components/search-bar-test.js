6a9bbd364ad4c98f3b2b67d70d91b4c8eb95d0f8
ekraihan <ekraihan@iupui.edu>
2017-12-11 00:46:16 -0500

    adding search bar test

--- tests/integration/components/search-bar-test.js
+++ tests/integration/components/search-bar-test.js
@@ -1,24 +1,29 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import { fillIn, triggerEvent } from 'ember-native-dom-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('search-bar', 'Integration | Component | search bar', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('Searching calls action and sets value', function(assert) {
+  const SEARCH_VALUE = "Search Value";
 
-  this.render(hbs`{{search-bar}}`);
+  this.on('typeIntoSearchBar', (value) => {
+    assert.equal(value, SEARCH_VALUE);
+  })
 
-  assert.equal(this.$().text().trim(), '');
+  this.set('typedValue', '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#search-bar}}
-      template block text
-    {{/search-bar}}
-  `);
+  this.render(hbs`{{search-bar
+    onInput=(action 'typeIntoSearchBar')
+    value=typedValue
+  }}`);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  Ember.run(() => {
+    fillIn(".search-bar input", SEARCH_VALUE);
+    triggerEvent(".search-bar input", "keyup");
+  })
+
+  assert.equal(this.get('typedValue'), SEARCH_VALUE)
 });
